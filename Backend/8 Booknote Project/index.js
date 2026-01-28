05c56a402091f05ce9325f004be1fea7dbd38d7f
Alex Ge-Hall <xan-y@outlook.com>
2026-01-28 18:15:10 +1000

    Complete project booknotes

--- Backend/8 Booknote Project/index.js
+++ Backend/8 Booknote Project/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { resolve } from "node:path";
 import morgan from "morgan";
 import pg from "pg";
-import axios from "axois";
+import axios from "axios";
 import "dotenv/config";
 
 const db = new pg.Pool({
@@ -24,19 +24,35 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(resolve(__dirname, "public")));
 
-async function getBooks() {
-  const result = await db.query("SELECT * FROM books");
-  return result.rows.map((row) => ({
-    title: row.title,
-    date: row.date,
-    rating: row.rating,
-    review: row.review,
-  }));
+async function getBooksWithCovers() {
+  try {
+    const result = await db.query(
+      "SELECT b.date, b.title,b.rating, b.review, i.isbn FROM book b JOIN book_isbn i ON b.title = i.title ORDER BY date DESC",
+    );
+    const books = result.rows;
+    console.log(books);
+
+    // get book covers
+    const bookCovers = await Promise.all(
+      books.map(async (book) => {
+        const bookcoverURL = `${process.env.API_URL}/isbn/${book.isbn}-M.jpg`;
+        return { ...book, cover: bookcoverURL };
+      }),
+    );
+    return bookCovers;
+  } catch (err) {
+    console.error("Database query error:", err.stack);
+    return [];
+  }
 }
 
 app.get("/", async (req, res) => {
-  const books = await getBooks();
-  res.render("index", { books: books });
+  try {
+    const books = await getBooksWithCovers();
+    res.render("index", { books });
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.listen(port, () => {
